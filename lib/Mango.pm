a490c942925fee5975065e776c07df540e2c4159
Sebastian Riedel <sri@cpan.org>
2013-08-17 22:31:34 +0200

    more consistent time tests

--- lib/Mango.pm
+++ lib/Mango.pm
@@ -23,7 +23,7 @@ has protocol        => sub { Mango::Protocol->new };
 has w               => 1;
 has wtimeout        => 1000;
 
-our $VERSION = '0.12';
+our $VERSION = '0.13';
 
 # Operations with reply
 for my $name (qw(get_more query)) {
