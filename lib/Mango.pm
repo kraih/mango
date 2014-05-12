fe7436f30c05e4d5dc068e44bb3f126a24313388
Sebastian Riedel <sri@cpan.org>
2014-05-12 19:42:30 +0200

    bump version

--- lib/Mango.pm
+++ lib/Mango.pm
@@ -24,7 +24,7 @@ has [qw(max_write_batch_size wtimeout)] => 1000;
 has protocol => sub { Mango::Protocol->new };
 has w => 1;
 
-our $VERSION = '0.38';
+our $VERSION = '0.39';
 
 sub DESTROY { shift->_cleanup }
 
